9323e1d21dfe01dd204a62b2adbdb4af16a00bd2
Raksa adhinata <raksaadhinata@gmail.com>
2026-06-20 13:16:54 +0700

    Commit pertama: Inisialisasi proyek

---
+++ test/RX.cpp
@@ -0,0 +1,25 @@
+#include <SPI.h>
+#include <RF24.h>
+
+RF24 radio(4, 5); // CE, CSN
+const byte address[6] = "TALLY";
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setChannel(102);
+  radio.startListening();
+  Serial.println("RX Siap, menunggu pesan...");
+}
+
+void loop() {
+  if (radio.available()) {
+    char text[32];
+    radio.read(&text, sizeof(text));
+    Serial.print("Pesan diterima: ");
+    Serial.println(text);
+  }
+}
---
+++ test/TX.cpp
@@ -0,0 +1,28 @@
+#include <SPI.h>
+#include <RF24.h>
+
+RF24 radio(4, 5); // CE, CSN
+const byte address[6] = "TALLY";
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setChannel(102);
+  radio.stopListening();
+  Serial.println("TX Siap, mencoba mengirim...");
+}
+
+void loop() {
+  const char text[] = "Tally On";
+  bool ok = radio.write(&text, sizeof(text));
+  
+  if (ok) {
+    Serial.println("Pesan terkirim sukses!");
+  } else {
+    Serial.println("Gagal kirim (Cek kabel/koneksi NRF)");
+  }
+  delay(1000);
+}
