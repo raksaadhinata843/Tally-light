f4c23d550f408ebbb65b9f5349eb87d7232a6fec
Raksa adhinata <raksaadhinata@gmail.com>
2026-06-30 19:31:25 +0700

    Add new ESP32 environments for RX and TX configurations; implement UDP communication and enhance LED control logic

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ const int MODE_SWITCH_PIN = 14;
 
 uint8_t broadcastAddress[4][6] = {
   {0x24,0xD7,0xEB,0xCD,0x27,0x3D},
-  {0x24,0xD7,0xEB,0xCD,0x27,0x4D},
+ // {0x20,0x50,0x0d,0xd2,0x38,0x8c},
   {0x24,0xD7,0xEB,0xCD,0x27,0x5D},
   {0x24,0xD7,0xEB,0xCD,0x27,0x6D}
 };
@@ -83,72 +83,45 @@ void loop() {
 #endif
 
 // ====================================================================
-// --- TX CONFIGURATION ESP32(ROUTER) (DUAL OUTPUT: ESP-NOW + nRF24L01) ---
+// --- TX CONFIGURATION ESP32(UDP) ---
 // ====================================================================
-
-#ifdef MODE_TX_ESP32ROUTER
+#ifdef MODE_TX_ESP32UDP
 #include <WiFi.h>
-#include <HTTPClient.h>
-#include <esp_now.h>
+#include <WiFiUdp.h>
 
 // --- KONFIGURASI ---
-const char* ssid = "TP-Link_9BFE"; 
-const char* password = "05674411";
-const char* ROUTERApi = "http://192.168.1.100:58088/api/"; // IP ROUTER kamu
+const char* ssid = "Rec.709"; 
+const char* password = "malammalam";
+const int udpPort = 8888;
+WiFiUDP udp;
 
-uint8_t broadcastAddress[] = {0x24, 0xD7, 0xEB, 0xCD, 0x27, 0x3D}; // RX1
+IPAddress local_IP(192, 168, 0, 5);
+IPAddress gateway(192, 168, 0, 1);
+
+const uint8_t PGM_PINS[4] = {12, 13, 25, 26}; 
+const uint8_t PVW_PINS[4] = {27, 32, 33, 34};
 
 TallyPacket txPacket;
-HTTPClient http;
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
-  http.setReuse(true);
-  http.setTimeout(100);
+  while (WiFi.status() != WL_CONNECTED) { 
+    delay(500); Serial.print("."); }
 
-  if (esp_now_init() != ESP_OK) return;
-  
-  esp_now_peer_info_t peerInfo = {};
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-  esp_now_add_peer(&peerInfo);
+  udp.begin(udpPort);
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin(vmixApi);
-    int httpCode = http.GET();
-    
-    if (httpCode == 200) {
-      String payload = http.getString();
-      
-      // Cari posisi <active>1</active>
-      // Kita ambil angka di antara tag tersebut
-      int startActive = payload.indexOf("<active>") + 8;
-      int endActive = payload.indexOf("</active>");
-      String activeStr = payload.substring(startActive, endActive);
-      int active = activeStr.toInt();
-      
-      // Cari posisi <preview>2</preview>
-      int startPreview = payload.indexOf("<preview>") + 9;
-      int endPreview = payload.indexOf("</preview>");
-      String previewStr = payload.substring(startPreview, endPreview);
-      int preview = previewStr.toInt();
-      
-      // LOGIC MASKING (Sama seperti sebelumnya)
-      txPacket.pgm_mask = (active > 0) ? (1 << (active - 1)) : 0;
-      txPacket.pvw_mask = (preview > 0) ? (1 << (preview - 1)) : 0;
-      
-      // KIRIM KE ESP-NOW
-      esp_now_send(broadcastAddress, (uint8_t*)&txPacket, sizeof(txPacket));
-      }
-    http.end();
+  txPacket.pgm_mask = 0;
+  txPacket.pvw_mask = 0;
+  for(int i = 0; i < 4; i++) {
+    if (digitalRead(PGM_PINS[i]) == LOW) txPacket.pgm_mask |= (1 << i);
+    if (digitalRead(PVW_PINS[i]) == LOW) txPacket.pvw_mask |= (1 << i);
   }
-  delay(50); // Latensi vMix API biasanya di kisaran 100ms, ini sudah sangat cukup
+  udp.beginPacket("192.168.0.255", udpPort);
+  udp.write((uint8_t*)&txPacket, sizeof(txPacket));
+  udp.endPacket();
 }
 #endif
 
@@ -217,6 +190,138 @@ void loop() {
 #endif
 
 // ====================================================================
+// --- RX CONFIGURATION ESP32---
+// ====================================================================
+#ifdef MODE_RX_ESP32
+#include <WiFi.h>
+#include <esp_now.h>
+
+#define RED 18
+#define GREEN 19
+#define BLUE 21
+
+// Tentukan ID Tally ini (Misal ID 1, ID 2, dst)
+const uint8_t CAM_ID = 2; 
+
+TallyPacket rxPacket;
+
+volatile uint8_t pgm_mask = 0;
+volatile uint8_t pvw_mask = 0;
+
+void OnDataRecv(uint8_t *mac, uint8_t *data, uint8_t len) {
+    if (len == sizeof(TallyPacket)) {
+        memcpy(&rxPacket, data, sizeof(TallyPacket));
+    }
+}
+
+void setup() {
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    
+    if (esp_now_init() != 0) return;
+}
+
+void loop() {
+    // Gunakan Bitwise untuk cek apakah ID ini ada di dalam mask
+    // (1 << (CAM_ID - 1)) akan menghasilkan bit yang sesuai untuk ID tersebut
+    bool isPgm = (rxPacket.pgm_mask & (2 << (CAM_ID - 2)));
+    bool isPvw = (rxPacket.pvw_mask & (2 << (CAM_ID - 2)));
+
+    if (isPgm) {
+        // Status PGM (Merah)
+        digitalWrite(RED, HIGH);
+        digitalWrite(GREEN, LOW);
+        digitalWrite(BLUE, LOW);
+    } else if (isPvw) {
+        // Status PVW (Hijau)
+        digitalWrite(RED, LOW);
+        digitalWrite(GREEN, HIGH);
+        digitalWrite(BLUE, LOW);
+    } else {
+        // Status IDLE (Biru)
+        digitalWrite(RED, LOW);
+        digitalWrite(GREEN, LOW);
+        digitalWrite(BLUE, HIGH);
+    }
+}
+#endif
+
+// ====================================================================
+// --- RX CONFIGURATION ESP32 (UDP)---
+// ====================================================================
+#ifdef MODE_RX_ESP32UDP
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+#define RED 25
+#define GREEN 26
+#define BLUE 27
+
+// Tentukan ID Tally ini (0, 1, 2, 3)
+const uint8_t CAM_ID = 0; 
+
+TallyPacket rxPacket;
+
+const char* ssid = "Rec.709";
+const char* password = "malammalam";
+const int udpPort = 8888;
+WiFiUDP udp;
+
+volatile uint8_t pgm_mask = 0;
+volatile uint8_t pvw_mask = 0;
+
+void OnDataRecv(uint8_t *mac, uint8_t *data, uint8_t len) {
+    if (len == sizeof(TallyPacket)) {
+        memcpy(&rxPacket, data, sizeof(TallyPacket));
+    }
+}
+
+void setup() {
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    
+    WiFi.begin(ssid, password);
+    
+    udp.begin(udpPort);
+}
+
+void loop() {
+    int packetSize = udp.parsePacket();
+    if (packetSize == sizeof(TallyPacket)) {
+        TallyPacket rxPacket;
+        udp.read((unsigned char*)&rxPacket, sizeof(TallyPacket));
+
+        // LOGIKA BITWISE YANG BENAR:
+        // Gunakan operator '&' untuk men-masking bit spesifik
+        bool isPgm = (rxPacket.pgm_mask & (1 << CAM_ID));
+        bool isPvw = (rxPacket.pvw_mask & (1 << CAM_ID));
+
+        if (isPgm) {
+            // PGM (Merah) - Prioritas utama
+            digitalWrite(RED, HIGH);
+            digitalWrite(GREEN, LOW);
+            digitalWrite(BLUE, LOW);
+        } else if (isPvw) {
+            // PVW (Hijau)
+            digitalWrite(RED, LOW);
+            digitalWrite(GREEN, HIGH);
+            digitalWrite(BLUE, LOW);
+        } else {
+            // IDLE (Biru)
+            digitalWrite(RED, LOW);
+            digitalWrite(GREEN, LOW);
+            digitalWrite(BLUE, HIGH);
+        }
+    }
+}
+#endif
+
+// ====================================================================
 // --- RX CONFIGURATION ESP8266(WS)---
 // ====================================================================
 #ifdef MODE_RX_ESP8266_WS
@@ -279,7 +384,6 @@ void loop() {
 // ====================================================================
 // --- TX CONFIGURATION ANANO ---
 // ====================================================================
-
 #ifdef MODE_TX_NANO
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -378,7 +482,6 @@ void loop() {
 // ====================================================================
 // --- RX CONFIGURATION ANANO ---
 // ====================================================================
-
 #ifdef MODE_RX_NANO
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -449,5 +552,104 @@ void loop() {
       digitalWrite(BLUE, HIGH);   // Biru jika Kamera tidak aktif (Idle)
     }
   }
+}
+#endif
+
+
+#ifdef MODE_TX_ESP32_MESH
+#include "esp_mesh.h"
+
+#define MESH_PREFIX   "Rec.709"
+#define MESH_PASSWORD "malammalam"
+#define MESH_PORT     5555
+
+painlessMesh mesh;
+
+void setup() {
+  Serial.begin(115200);
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+}
+
+void loop() {
+  mesh.update();
+
+  static unsigned long lastSend = 0;
+  if (millis() - lastSend > 3000) { // Kirim perintah tiap 3 detik
+    
+    // CONTOH PERINTAH: Kita mau suruh RX ID 2 menyalakan warna MERAH dan BIRU (Ungu)
+    uint8_t targetRX = 2;       // ID 2  -> (Binary: 0010)
+    uint8_t warnaRGB = 0b101;   // R=1, G=0, B=1 (Ungu)
+    
+    // Gabungkan menggunakan bitwise OR dan Shift Left
+    // Geser ID target ke 4 bit kiri, lalu gabungkan dengan warna
+    uint8_t dataKirim = (targetRX << 4) | warnaRGB; 
+
+    // Kirim data secara broadcast ke semua mesh
+    mesh.sendBroadcast(String(dataKirim));
+    
+    Serial.printf("TX Mengirim Data: %d (Binary: 0b", dataKirim);
+    for (int i = 7; i >= 0; i--) Serial.print((dataKirim >> i) & 1);
+    Serial.println(")");
+
+    lastSend = millis();
+  }
+}
+#endif
+
+#ifdef MODE_RX_ESP32_MESH
+#include <esp_mesh.h>
+
+#define MESH_PREFIX   "Rec.709"
+#define MESH_PASSWORD "malammalam"
+#define MESH_PORT     5555
+
+// !!! UBAH ID INI DI SETIAP BOARD RX (1 sampai 15) !!!
+#define MY_RX_ID      2  
+
+// Definisikan Pin LED RGB (Sesuaikan dengan pin ESP32/ESP8266 lu)
+#define PIN_R         25 
+#define PIN_G         26
+#define PIN_B         27
+
+painlessMesh mesh;
+
+// Fungsi masking untuk membaca data masukan
+void receivedCallback(uint32_t from, String &msg) {
+  uint8_t dataDiterima = msg.toInt();
+
+  // 1. Ekstrak ID Target menggunakan Bitmask 0xF0 (0b11110000) dan geser kanan 4 kali
+  uint8_t targetID = (dataDiterima & 0xF0) >> 4;
+
+  // Cek apakah data ini memang ditujukan untuk RX ini
+  if (targetID == MY_RX_ID) {
+    Serial.printf("Data cocok untuk RX [%d]! Memproses warna...\n", MY_RX_ID);
+
+    // 2. Ekstrak status RGB menggunakan Bitmask 0x07 (0b00000111)
+    bool statusR = dataDiterima & 0x04; // Cek Bit 2 (Red)  -> 0b100
+    bool statusG = dataDiterima & 0x02; // Cek Bit 1 (Green)-> 0b010
+    bool statusB = dataDiterima & 0x01; // Cek Bit 0 (Blue) -> 0b001
+
+    // 3. Eksekusi ke Pin Hardware (Asumsi LED Common Cathode / Aktif HIGH)
+    digitalWrite(PIN_R, statusR ? HIGH : LOW);
+    digitalWrite(PIN_G, statusG ? HIGH : LOW);
+    digitalWrite(PIN_B, statusB ? HIGH : LOW);
+    
+    Serial.printf("Status Output -> R:%d G:%d B:%d\n", statusR, statusG, statusB);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(PIN_R, OUTPUT);
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+  mesh.onReceive(&receivedCallback);
+}
+
+void loop() {
+  mesh.update();
 }
 #endif
